3231d0c1968e90794b7eb89449fa5b0f1a117726
Sławomir Majchrzak <smmaya@yahoo.fr>
2022-04-12 22:26:44 +0200

    second commit

--- src/app/cw1/cw1.component.ts
+++ src/app/cw1/cw1.component.ts
@@ -7,24 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Cw1Component implements OnInit {
 
-  inputText = 'Moj input';
+  inputText = ' Moj input';
   zmienKolor = 'color-black';
   isDisabled = true;
+  changeButtonColor = 'button-color-before';
+  inputText2 = '';
+  inputText3 = '';
 
+  update(value: string) { 
+    this.inputText3 = value; 
+  }
   zmienUstawienia() {
     this.isDisabled = false;
+  }
+  zmienKolorButona(){
+    this.changeButtonColor = 'button-color-before';
   }
   onFocus() {
     this.zmienKolor = 'color-brown';
   }
   onClick(event: any) {
     console.log(event);
+    this.changeButtonColor = 'button-color-after';
   }
   onMouseMove(event: any) {
     console.log('punktX: ' + event.clientX + ' punktY: ' + event.clientY);
   }
   onPaste() {
-    this.inputText = 'Wklejanie zabronione!';
+    this.inputText = ' Wklejanie zabronione!';
   }
 
   constructor() { }
