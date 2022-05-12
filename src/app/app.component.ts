9360618e9c4179c0d600a989b805e67fe18d5d2f
Sławomir Majchrzak <smmaya@yahoo.fr>
2022-05-12 08:06:16 +0200

    poprawa błędu dodawania samochodu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,38 +9,39 @@ import { Samochod } from './samochod';
 export class AppComponent {
   title = 'projekt3';
   studenci: string[];
+  samochody: Samochod[] = [];
   isCreated = false;
-
-  samochody: Samochod[] = [
-    {
-      marka: 'Audi',
-      model: 'RS7',
-      rok: 2020
-    },
-    {
-      marka: 'Mercedes',
-      model: 'GT',
-      rok: 1998
-    },
-    {
-      marka: 'Mercedes',
-      model: 'Klasa G',
-      rok: 2018
-    },
-    {
-      marka: 'BMW',
-      model: 'Seria 3',
-      rok: 2020
-    },
-    {
-      marka: 'Nissan',
-      model: '370z',
-      rok: 2019
-    }
-  ];
+  isInArray = false;
 
   ngOnInit(): void {
     this.studenci = ['Karol', 'Basia', 'Adam', 'Teresa', 'Wojciech', 'Ziemowit'];
+    this.samochody = [
+      {
+        marka: 'Audi',
+        model: 'RS7',
+        rok: 2020
+      },
+      {
+        marka: 'Mercedes',
+        model: 'GT',
+        rok: 1998
+      },
+      {
+        marka: 'Mercedes',
+        model: 'Klasa G',
+        rok: 2018
+      },
+      {
+        marka: 'BMW',
+        model: 'Seria 3',
+        rok: 2020
+      },
+      {
+        marka: 'Nissan',
+        model: '370z',
+        rok: 2019
+      }
+    ];
   }
 
   onAddStudent(student: string) {
@@ -57,17 +58,25 @@ export class AppComponent {
     this.studenci.splice(index, 1);
   }
 
+
   onAddSamochod(samochod: Samochod) {
-    if (this.samochody.includes(samochod)) {
-      this.isCreated = true;
+    
+    var areObjectEquals = (obj1) =>
+      obj1.marka === samochod.marka &&
+      obj1.model === samochod.model &&
+      obj1.rok === samochod.rok;
+
+    if (this.samochody.some(areObjectEquals, samochod.marka && samochod.model && samochod.rok)) {
+      this.isInArray = true;
     }
     else {
-      this.isCreated = false;
+      this.isInArray = false;
       this.samochody.push(samochod);
     }
+
   }
   onUsunSamochod(index: number) {
-    this.isCreated = false;
+    this.isInArray = false;
     this.samochody.splice(index, 1);
   }
 }
